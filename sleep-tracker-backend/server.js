a09cb7c9bd5267720e2e081d878816456fc8d6ce
Sivasai48 <paravadasivasai@gmail.com>
2025-09-18 10:57:27 +0530

    url changes done

--- sleep-tracker-backend/server.js
+++ sleep-tracker-backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
+require('dotenv').config();
+
 
 const app = express();
 
@@ -10,16 +12,16 @@ const app = express();
 const allowedOrigins = ["http://localhost:8080", "http://localhost:8086" ,"https://sleeptracker-projects-frontend.vercel.app"];
 
 app.use(cors({
-  origin: function (origin, callback) {
+  origin: function(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS: " + origin));
     }
   },
   credentials: true
 }));
-
+app.set('trust proxy', 1);
 app.use(express.json());
 
 app.use(session({
@@ -33,12 +35,13 @@ app.use(session({
 }));
 
 // ✅ MongoDB connection
-const url="mongodb://localhost:27017/sleep_tracker";
-mongoose.connect(url, {
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("MongoDB Error:", err));
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.error("MongoDB Error:", err));
 
 // ✅ Schemas and Models
 const UserSchema = new mongoose.Schema({
@@ -112,5 +115,5 @@ app.delete("/api/sleep/:id", requireAuth, async (req, res) => {
 });
 
 // ✅ Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
